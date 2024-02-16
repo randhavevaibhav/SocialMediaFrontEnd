b3c51bea0c96945f5ec8546643cea95bee07a911
randhavevaibhav <randhavevaibhav3@gmail.com>
2024-02-16 12:04:15 +0530

    Added source files completed home,login,register pages

---
+++ src/scenes/homePage/index.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@mui/material";
+import NavBar from "../navbar/index";
+
+const HomePage = ()=>{
+    return <Box><NavBar/></Box>
+}
+
+export default HomePage; 
---
+++ src/scenes/profilePage/index.jsx
@@ -0,0 +1,5 @@
+const ProfilePage = ()=>{
+    return <div>Login Page</div>
+}
+
+export default ProfilePage;
